cfc34faad0a85191403dc56900652eefb35e2b9d
Tom MacWright <tom@macwright.org>
2014-10-23 18:33:06 -0400

    Tests and package

--- test.js
+++ test.js
@@ -1,6 +1,15 @@
 var test = require('tape'),
   BaseCore = require('./');
 
+if (!localStorage.bcUser) localStorage.bcUser = prompt('user');
+if (!localStorage.bcPassword) localStorage.bcPassword = prompt('password');
+
+var user = localStorage.bcUser,
+    pw = localStorage.bcPassword;
+
 test('BaseCore', function(t) {
-    t.end();
+    var bc = new BaseCore('https://122e4e-mapbox.global.ssl.fastly.net');
+    bc.login(user, pw, function() {
+        t.end();
+    });
 });
