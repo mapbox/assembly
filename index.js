a664ccbc2f494ed4e18a5d14e9eee3293621969a
Tom MacWright <tom@macwright.org>
2014-10-27 11:09:13 -0400

    Add tests

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 var xhr = require('xhr');
 
 function BaseCore(api) {
-    this.api = api;
+  this.api = api;
 }
 
-BaseCore.prototype.login = function(username, password, callback) {
-    var endpoint = this.api + '/api/login?_=' + Date.now();
-    xhr({
-        uri: endpoint
-    }, function(err, resp, body) {
-    });
+BaseCore.prototype.signin = function(username, password, callback) {
+  xhr({
+    uri: this.api + '/api/login?_=' + Date.now(),
+    json: {
+      username: username.toLowerCase(),
+      password: password
+    },
+    method: 'POST'
+  }, function(err, resp, body) {
+    if (err) return callback(err);
+    else callback(null, body);
+  });
+};
+
+BaseCore.prototype.signout = function(callback) {
+  xhr({
+    uri: this.api + '/api/logout?_=' + Date.now(),
+    method: 'POST',
+    withCredentials: true
+  }, function(err, resp, body) {
+    if (err) return callback(err);
+    else callback(null, body);
+  });
+};
+
+BaseCore.prototype.getUser = function(callback) {
+  xhr({
+    uri: this.api + '/api/session',
+    withCredentials: true,
+    json: {}
+  }, function(err, resp, body) {
+    if (err) return callback(err);
+    else callback(null, body);
+  });
 };
 
 module.exports = BaseCore;
--- test.js
+++ test.js
@@ -7,9 +7,24 @@ if (!localStorage.bcPassword) localStorage.bcPassword = prompt('password');
 var user = localStorage.bcUser,
     pw = localStorage.bcPassword;
 
-test('BaseCore', function(t) {
-    var bc = new BaseCore('https://122e4e-mapbox.global.ssl.fastly.net');
-    bc.login(user, pw, function() {
-        t.end();
+test('base-core#signin', function(t) {
+  var bc = new BaseCore('https://122e4e-mapbox.global.ssl.fastly.net');
+  bc.signin(user, pw, function(err, user) {
+    t.equal(err, null, 'No error returned');
+    t.ok(user, 'User returned');
+    bc.getUser(function(err, userB) {
+      t.equal(err, null, 'No error returned');
+      t.equal(user._id, userB._id, 'Same user returned');
+      t.end();
     });
+  });
+});
+
+test('base-core#signout', function(t) {
+  var bc = new BaseCore('https://122e4e-mapbox.global.ssl.fastly.net');
+  bc.signout(function(err, user) {
+    t.equal(err, null, 'No error returned');
+    t.equal(user, null, 'No error returned');
+    t.end();
+  });
 });
