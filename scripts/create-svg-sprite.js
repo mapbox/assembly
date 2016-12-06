9ee8cb573060857868768135484bcebc05733ee0
David Clark <dclark@mapbox.com>
2016-12-06 12:08:05 -0500

    Loading

--- scripts/create-svg-sprite.js
+++ scripts/create-svg-sprite.js
@@ -2,6 +2,7 @@
 
 const svgstore = require('svgstore');
 const fs = require('fs');
+const _ = require('lodash');
 const path = require('path');
 const queue = require('d3-queue').queue;
 const SVGO = require('svgo');
@@ -16,12 +17,19 @@ const sprite = svgstore({
   ]
 });
 
+const baseJsTemplate = _.template(
+`var svgDocument = (new DOMParser()).parseFromString('<%= svgSprite %>', 'text/xml');
+document.addEventListener("DOMContentLoaded", function() {
+  document.body.appendChild(svgDocument.getElementById("svg-symbols"));
+});`
+);
+
 function addFileToSprite(filename, callback) {
   const basename = path.basename(filename, path.extname(filename));
   fs.readFile(filename, 'utf8', (err, content) => {
     if (err) throw err;
     svgo.optimize(content, (optimizedContent) => {
-      sprite.add(`icon-${basename}`, content);
+      sprite.add(`base-svg-${basename}`, content);
       callback();
     });
   });
@@ -37,6 +45,13 @@ fs.readdir(svgDir, (err, filenames) => {
 
   q.awaitAll((err) => {
     if (err) throw err;
-    fs.writeFileSync(path.join(__dirname, '../dist/svgs.svg'), sprite);
+
+    sprite.element('svg')
+      .attr('id', 'svg-symbols')
+      .attr('style', 'display:none');
+
+    const cleanedSprite = sprite.toString().replace(/\n/g, '');
+    const jsContent = baseJsTemplate({ svgSprite: cleanedSprite });
+    fs.writeFileSync(path.join(__dirname, '../dist/base.js'), jsContent);
   });
 });
