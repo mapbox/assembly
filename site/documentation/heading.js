70936da792e16be45d46ad4e817ec99c32a543d9
David Clark <dclark@mapbox.com>
2016-12-19 14:57:14 -0800

    Document color variants

--- site/documentation/heading.js
+++ site/documentation/heading.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import remark from 'remark';
 import reactRenderer from 'remark-react';
+import { HtmlExample } from '../html_example';
 
 class Heading extends React.Component {
   render() {
@@ -9,14 +10,18 @@ class Heading extends React.Component {
     const id = props.title.replace(/\s+/g, '-');
 
     let descriptionEl;
-    if (props.description) {
+    if (props.parsedComment.description) {
       descriptionEl = <div className='prose'>
-        {remark().use(reactRenderer).process(props.description).contents}
+        {remark().use(reactRenderer).process(props.parsedComment.description).contents}
       </div>;
     }
 
     const sectionClass = props.level === 1 ? 'mt24 pb18 border--gray' : 'pt12 pb12 mt12 border--gray-faint';
     const levelClass = props.level === 1 ? 'txt-subhead mb12' : 'txt-xl mb6';
+
+    const example = !props.parsedComment.example ? null : (
+      <HtmlExample code={props.parsedComment.example.description} />
+    );
 
     return (
       <div className={`mb48 border-b border--2 ${sectionClass}`}>
@@ -29,6 +34,7 @@ class Heading extends React.Component {
           </a>
         </div>
         {descriptionEl}
+        {example}
       </div>
     );
   }
@@ -37,7 +43,9 @@ class Heading extends React.Component {
 Heading.propTypes = {
   level: React.PropTypes.oneOf([1, 2, 3, 4, 5]).isRequired,
   title: React.PropTypes.string.isRequired,
-  description: React.PropTypes.string
+  parsedComment: React.PropTypes.shape({
+    description: React.PropTypes.string
+  }).isRequired
 };
 
 export { Heading };
