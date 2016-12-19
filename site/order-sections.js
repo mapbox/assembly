46e111251091137ea5010589c32d8afce52f8280
David Clark <dclark@mapbox.com>
2016-12-19 09:20:23 -0800

    Order sections
    Closes #342

---
+++ site/order-sections.js
@@ -0,0 +1,40 @@
+import _ from 'lodash';
+
+function orderSections(entries) {
+  function getSection(title) {
+    const sectionEntry = entries.find((entry) => {
+      if (!entry.title) return false;
+      return entry.title.toLowerCase() === title.toLowerCase();
+    });
+
+    if (sectionEntry === undefined) {
+      throw new Error(`Could not find entry with title "${title}"`);
+    }
+
+    if (sectionEntry.members) {
+      sectionEntry.members = _.sortBy(sectionEntry.members, 'title');
+    }
+
+    return sectionEntry;
+  }
+
+  // We need to add to this list when we
+  // add new primary sections
+  const sections = [
+    getSection('typography'),
+    getSection('layout'),
+    getSection('theming'),
+    getSection('colors & gradients'),
+    getSection('icons'),
+    getSection('buttons'),
+    getSection('forms'),
+    getSection('tables'),
+    getSection('animations'),
+    getSection('triangles'),
+    getSection('miscellaneous'),
+  ];
+
+  return sections;
+}
+
+export { orderSections };
