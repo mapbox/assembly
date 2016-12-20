3caca21481e797d885e0d75078d7d9b5f98a89f1
tristen <tristen.brown@gmail.com>
2016-12-19 19:56:36 -0500

    remove log & early return

--- test/build-color-variants.jest.js
+++ test/build-color-variants.jest.js
@@ -2,9 +2,6 @@
 
 const buildColorVariants = require('../scripts/build-color-variants');
 
-console.log(buildColorVariants());
-return;
-
 describe('buildColorVariants', () => {
   it('defaults', () => {
     expect(buildColorVariants()).toMatchSnapshot();
