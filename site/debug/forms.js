d67ffa6ac3afc94027e00e985c3b56ad55d9853f
Katy DeCorah <katydecorah@gmail.com>
2016-12-21 10:08:05 -0500

    improve range--dark

--- site/debug/forms.js
+++ site/debug/forms.js
@@ -86,26 +86,31 @@ class Forms extends React.Component {
               </label>
             </div>
           </fieldset>
-          <fieldset className='mt30'>
+          <fieldset className='mt24'>
             <legend className='txt-bold'>Range</legend>
             <div className='mt6'>
               <label className='range-container'>
-                Range <input className='range' name='range' min='0' max='100' type='range' />
+                Range <input className='range w96' name='range' min='0' max='100' type='range' />
               </label>
             </div>
             <div className='mt6'>
               <label className='range-container'>
-                Range Small <input className='range range--s' name='range' min='0' max='100' type='range' />
+                Range Small <input className='range range--s w96' name='range' min='0' max='100' type='range' />
               </label>
             </div>
             <div className='mt6 p6 bg-gray-dark color-white'>
               <label className='range-container'>
-                Range dark <input className='range range--dark' name='range' min='0' max='100' type='range' />
+                Range dark <input className='range range--dark w96' name='range' min='0' max='100' type='range' />
+              </label>
+            </div>
+            <div className='mt6 p6 bg-red-dark color-white'>
+              <label className='range-container'>
+                Range dark <input className='range range--dark w96' name='range' min='0' max='100' type='range' />
               </label>
             </div>
             <div className='mt6'>
               <label className='range-container'>
-                 Range color example <input className='range range--purple' name='range' min='0' max='100' type='range' />
+                 Range color example <input className='range range--purple w96' name='range' min='0' max='100' type='range' />
               </label>
             </div>
           </fieldset>
