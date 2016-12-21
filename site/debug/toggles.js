0585cc31a65fcbf13ec61b7149eb58707b584723
samanpwbb <samanpwbb@gmail.com>
2016-12-20 20:22:04 -0500

    add rules for setting active toggle color

--- site/debug/toggles.js
+++ site/debug/toggles.js
@@ -32,24 +32,24 @@ const lightenColors = [
 ];
 
 function ToggleEl(props) {
-  const toggleClass = `toggle toggle--${props.color}`;
-  const toggleGroupClass = `mr18 toggle-group ${props.small ? 'toggle-group--s txt-s' : ''} ${props.stroke ? 'toggle-group--stroke color-' + props.color : ''}`;
+  const toggleClass = `toggle toggle--active-${props.activeColor} toggle--${props.color}`;
+  const toggleGroupClass = `mb12 mr12 toggle-group ${props.small ? 'toggle-group--s txt-s' : ''} ${props.stroke ? 'toggle-group--stroke color-' + props.color : ''}`;
   return (
     <div className={toggleGroupClass}>
       <label className='toggle-container'>
-        <input disabled={props.disabled} name={`animal-${props.color}-${props.stroke}-${props.small}-${props.disabled}`} value='cow' type='radio' />
+        <input disabled={props.disabled} name={`animal-${props.color}-${props.stroke}-${props.small}-${props.disabled}-${props.activeColor}`} value='cow' type='radio' />
         <div className={toggleClass}>
         cow
         </div>
       </label>
       <label className='toggle-container'>
-        <input checked disabled={props.disabled} name={`animal-${props.color}-${props.stroke}-${props.small}-${props.disabled}`} value='horse' type='radio' />
+        <input checked disabled={props.disabled} name={`animal-${props.color}-${props.stroke}-${props.small}-${props.disabled}-${props.activeColor}`} value='horse' type='radio' />
         <div className={toggleClass}>
         horse
         </div>
       </label>
       <label className='toggle-container'>
-        <input disabled={props.disabled} name={`animal-${props.color}-${props.stroke}-${props.small}-${props.disabled}`} value='pig' type='radio' />
+        <input disabled={props.disabled} name={`animal-${props.color}-${props.stroke}-${props.small}-${props.disabled}-${props.activeColor}`} value='pig' type='radio' />
         <div className={toggleClass}>
         pig
         </div>
@@ -67,9 +67,9 @@ class Toggles extends React.Component {
           Toggles
         </h1>
 
-        <div className='mb12'>
-        {colors.map((c) => <ToggleEl key={c} color={c} />)}
-        </div>
+        {colors.map((o) => <div key={o} className='mb12'>
+          {colors.map((c) => <ToggleEl key={c} color={c} activeColor={o} />)}
+        </div>)}
 
         <div className='mb12'>
         {colors.map((c) => <ToggleEl key={c} color={c} disabled={true} />)}
@@ -86,6 +86,7 @@ class Toggles extends React.Component {
         <div className='mb12'>
         {colors.map((c) => <ToggleEl key={c} color={c} small={true} stroke={true} />)}
         </div>
+
 
         <div className='bg-blue p12 mt12'>
         {lightenColors.map((c) => <ToggleEl key={c} color={c} />)}
