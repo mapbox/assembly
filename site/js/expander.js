b3e3f0c54e67ba99d158fa12f437bd6436adaa8b
Dana Sulit <dana.sulit@mapbox.com>
2016-12-20 20:53:43 -0500

    indent fix?

--- site/js/expander.js
+++ site/js/expander.js
@@ -7,20 +7,20 @@ let i;
 
 for (i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function () {
-
+  
     const classId = this.id.replace('button-', '');
-  	const expanded = document.getElementById('expanded-' + classId);
-  	const collapsed = document.getElementById('collapsed-' + classId);
-
-  	if (expanded.className === 'none') {
-  	  expanded.className = 'inline';
+    const expanded = document.getElementById('expanded-' + classId);
+    const collapsed = document.getElementById('collapsed-' + classId);
+    
+    if (expanded.className === 'none') {
+      expanded.className = 'inline';
       collapsed.className = 'none';
-  	  this.textContent = 'see fewer';
-  	} else if (expanded.className === 'inline') {
-  	  expanded.className = 'none';
+      this.textContent = 'see fewer';
+    } else if (expanded.className === 'inline') {
+      expanded.className = 'none';
       collapsed.className = 'inline';
-  	  this.textContent = 'see all';
+      this.textContent = 'see all';
     }
-
+  
   });
 }
