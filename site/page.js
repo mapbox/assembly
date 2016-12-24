139d6f44b51222617b7688f78229ad3c46cd672c
samanpwbb <samanpwbb@gmail.com>
2016-12-23 23:40:04 -0500

    center page on large screens, closes #456

--- site/page.js
+++ site/page.js
@@ -8,7 +8,7 @@ class Page extends React.Component {
         <div className='bg-darken5 pt24 pr18 pb24 pl24 viewport-full-mm scroll-auto w180-mm fixed-mm top left'>
           <Navigation navData={this.props.navData} />
         </div>
-        <div className='mb24 ml180-mm'>
+        <div className='mb24 ml180-mm flex-parent flex-parent--center-main'>
           <div className='pl48 pr48 wmax1200 mb48'>
             {this.props.children}
           </div>
