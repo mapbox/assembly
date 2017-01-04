3cf2df679a2797eff2bed496fb16aad094d4d678
Dana Sulit <dana.sulit@mapbox.com>
2017-01-04 18:43:20 -0500

    remove flex classes from buttons and recreate pills

--- site/catalog/pills.js
+++ site/catalog/pills.js
@@ -21,21 +21,17 @@ class Pills extends React.Component {
           <button className='btn btn--red btn--stroke btn--pill btn--pill-hr round'>Confirm</button>
         </div>
 
-        <div className='mb18'>
-          <div className='flex-parent-inline flex-parent--column'>
-            <button className='btn btn--orange btn--s btn--pill btn--pill-vt'>Confirm</button>
-            <button className='btn btn--orange btn--s btn--pill btn--pill-vc'>Confirm</button>
-            <button className='btn btn--orange btn--s btn--pill btn--pill-vb is-active'>Confirm</button>
-          </div>
+        <div className='mb18 flex-parent-inline flex-parent--column'>
+          <button className='btn btn--orange btn--s btn--pill btn--pill-vt'>Confirm</button>
+          <button className='btn btn--orange btn--s btn--pill btn--pill-vc'>Confirm</button>
+          <button className='btn btn--orange btn--s btn--pill btn--pill-vb is-active'>Confirm</button>
         </div>
 
-        <div className='mb18'>
-          <div className='flex-parent-inline flex-parent--column'>
-            <button className='btn btn--stroke btn--s btn--pill btn--pill-vt round'>Confirm</button>
-            <button className='btn btn--stroke btn--s btn--pill btn--pill-vc round'>Confirm</button>
-            <button className='btn btn--stroke btn--s btn--pill btn--pill-vc round'>Confirm</button>
-            <button className='btn btn--stroke btn--s btn--pill btn--pill-vb round is-active'>Confirm</button>
-          </div>
+        <div className='mb18 ml18 flex-parent-inline flex-parent--column'>
+          <button className='btn btn--stroke btn--s btn--pill btn--pill-vt round'>Confirm</button>
+          <button className='btn btn--stroke btn--s btn--pill btn--pill-vc round'>Confirm</button>
+          <button className='btn btn--stroke btn--s btn--pill btn--pill-vc round'>Confirm</button>
+          <button className='btn btn--stroke btn--s btn--pill btn--pill-vb round is-active'>Confirm</button>
         </div>
       </div>
     );
