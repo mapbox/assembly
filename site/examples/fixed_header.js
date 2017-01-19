b757341c09ea40c8e46d7c8d4d3bd29fa8f3669d
Sarah Kleinman <kleinman.sarah@gmail.com>
2017-01-19 18:54:01 -0500

    Add fixed header example

---
+++ site/examples/fixed_header.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { HtmlExample } from '../html_example';
+
+const fixed_header = `<div class='bg-darken10 viewport-half'>
+<!-- In practice, it makes sense to use this for the container instead:
+<div class='fixed top right bottom left scroll-auto'></div>-->
+  <div class='flex-parent p12 bg-blue txt-m txt-bold color-white'>Fixed header</div>
+</div>`;
+
+class ExampleFixedHeader extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
+          Fixed header
+        </h2>
+        <HtmlExample code={fixed_header} />
+      </div>
+    );
+  }
+}
+
+export { ExampleFixedHeader };
