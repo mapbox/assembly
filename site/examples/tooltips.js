5a34d447ac64df84f818ee8fe0493c9040a78270
Sarah Kleinman <kleinman.sarah@gmail.com>
2017-01-19 17:47:50 -0500

    Change class names

--- site/examples/tooltips.js
+++ site/examples/tooltips.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import { HtmlExample } from '../html_example';
 
-const basic = `<div class='flex-parent-inline flex-parent--center-main flex-parent--wrap'>
+const basic = 
+`<div class='flex-parent-inline flex-parent--center-main flex-parent--wrap'>
   <div class='bg-darken75 color-white align-center col col--12 p6 round txt-bold txt-s'>Hello world!</div>
   <span class='triangle triangle--d'></span>
 </div>`;
+
+const basic_left = 
+`<div class='flex-parent-inline flex-parent--center-cross'>
+  <span class='triangle triangle--l'></span>
+  <div class='bg-darken75 color-white align-center col col--12 p6 round txt-bold txt-s'>Hello world!</div>
+</div>`;
+
+const basic_right = 
+`<div class='flex-parent-inline flex-parent--center-cross'>
+  <div class='bg-darken75 color-white align-center col col--12 p6 round txt-bold txt-s'>Hello world!</div>
+  <span class='triangle triangle--r'></span>
+</div>`
 
 const basic_with_closure = 
 `<div class='flex-parent-inline flex-parent--center-main flex-parent--wrap'>
@@ -17,18 +30,6 @@ const basic_with_closure =
   <span class='triangle triangle--d color-gray-faint'></span>
 </div>`;
 
-const side_tooltip_left = 
-`<div class='flex-parent-inline flex-parent--center-cross'>
-  <span class='triangle triangle--l'></span>
-  <div class='bg-darken75 color-white align-center col col--12 p6 round txt-bold txt-s'>Hello world!</div>
-</div>`;
-
-const side_tooltip_right = 
-`<div class='flex-parent-inline flex-parent--center-cross'>
-  <div class='bg-darken75 color-white align-center col col--12 p6 round txt-bold txt-s'>Hello world!</div>
-  <span class='triangle triangle--r'></span>
-</div>`
-
 
 class ExampleTooltips extends React.Component {
   render() {
@@ -39,17 +40,17 @@ class ExampleTooltips extends React.Component {
         </h2>
         <HtmlExample code={basic} />
         <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
+          Basic tooltip (left)
+        </h2>
+        <HtmlExample code={basic_left} />
+        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
+          Basic tooltip (right)
+        </h2>
+        <HtmlExample code={basic_right} />
+        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
           Tooltip with close button
         </h2>
         <HtmlExample code={basic_with_closure} />
-        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
-          Side tooltip--left
-        </h2>
-        <HtmlExample code={side_tooltip_left} />
-        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
-          Side tooltip--right
-        </h2>
-        <HtmlExample code={side_tooltip_right} />
       </div>
     );
   }
