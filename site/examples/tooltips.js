aa4572f3b3cffa18a8fc30bef580b483859c3db6
Sarah Kleinman <kleinman.sarah@gmail.com>
2017-01-19 16:27:51 -0500

    Add tooltip example with close button

--- site/examples/tooltips.js
+++ site/examples/tooltips.js
@@ -6,6 +6,17 @@ const basic = `<div class='flex-parent-inline flex-parent--center-main flex-pare
   <span class='triangle triangle--d'></span>
 </div>`;
 
+const basic_with_closure = 
+`<div class='flex-parent-inline flex-parent--center-main flex-parent--wrap'>
+  <div class='bg-darken5 color-darken50 align-center col col--12 p18 round txt-bold txt-s relative'>
+    <button class='absolute top right'>
+      <svg class='icon link color-darken50'><use xlink:href='#icon-close'></use></svg>
+    </button>
+    Hello world!
+  </div>
+  <span class='triangle triangle--d color-gray-faint'></span>
+</div>`;
+
 class ExampleTooltips extends React.Component {
   render() {
     return (
@@ -14,6 +25,10 @@ class ExampleTooltips extends React.Component {
           Basic tooltip
         </h2>
         <HtmlExample code={basic} />
+        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
+          Tooltip with close button
+        </h2>
+        <HtmlExample code={basic_with_closure} />
       </div>
     );
   }
