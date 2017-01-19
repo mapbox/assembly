338c38e04d2f9c209e7e56806af3594870b715d5
David Clark <dclark@mapbox.com>
2017-01-19 09:48:10 -0700

    Remove flex-shrink: 0 from flex-child

---
+++ site/catalog/flexbox.js
@@ -0,0 +1,63 @@
+import React from 'react';
+
+class Flexbox extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-uppercase txt-bold'>
+          Flexbox
+        </h2>
+
+        <div className='mb24'>
+          <div className='mb12 txt-bold color-darken50 txt-uppercase txt-s'>Standard flex children</div>
+          <div className='border flex-parent'>
+            <div className='flex-child bg-gray-light'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+            <div className='flex-child bg-gray-faint'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+          </div>
+        </div>
+
+        <div className='mb24'>
+          <div className='mb12 txt-bold color-darken50 txt-uppercase txt-s'>Set-width flex children</div>
+          <div className='border flex-parent'>
+            <div className='flex-child w300 bg-gray-light'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+            <div className='flex-child w420 bg-gray-faint'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+          </div>
+        </div>
+
+        <div className='mb24'>
+          <div className='mb12 txt-bold color-darken50 txt-uppercase txt-s'>One set-width flex child, the other with long text</div>
+          <div className='border flex-parent'>
+            <div className='flex-child w300 bg-gray-light'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+            <div className='flex-child bg-gray-faint'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+          </div>
+        </div>
+
+        <div className='mb24'>
+          <div className='mb12 txt-bold color-darken50 txt-uppercase txt-s'>One set-width flex child, the other with short text</div>
+          <div className='border flex-parent'>
+            <div className='flex-child w300 bg-gray-light'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+            <div className='flex-child bg-gray-faint'>Duis mollis.</div>
+          </div>
+        </div>
+
+        <div className='mb24'>
+          <div className='mb12 txt-bold color-darken50 txt-uppercase txt-s'>One set-width flex child and a growing child with long text</div>
+          <div className='border flex-parent'>
+            <div className='flex-child w300 bg-gray-light'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+            <div className='flex-child flex-child--grow bg-gray-faint'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+          </div>
+        </div>
+
+        <div className='mb24'>
+          <div className='mb12 txt-bold color-darken50 txt-uppercase txt-s'>One set-width flex child and a growing child with short text</div>
+          <div className='border flex-parent'>
+            <div className='flex-child w300 bg-gray-light'>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Donec ullamcorper nulla non metus auctor fringilla. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</div>
+            <div className='flex-child flex-child--grow bg-gray-faint'>Duis mollis.</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export { Flexbox };
