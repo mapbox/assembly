0c7c0e6d8ad91a370df590bb8bdd0239b1a3a300
tristen <tristen.brown@gmail.com>
2017-01-20 16:12:59 -0500

    Fix up classes in sidebar layout examples

--- site/examples/sidebar_apps.js
+++ site/examples/sidebar_apps.js
@@ -3,12 +3,12 @@ import { HtmlExample } from '../html_example';
 
 const basicSidebar = `<div class='flex-parent viewport-full relative clip'>
   <div class='flex-child w-full w300-ml absolute static-ml left bottom'>
-    <div class='viewport-third h-full hmax-full bg-white'>
-      <div class='flex-parent flex-parent--column p12 scroll-auto h-full'>
+    <div class='flex-parent flex-parent--column viewport-third h-full hmax-full bg-white'>
+      <div class='flex-child flex-child--grow p12 scroll-auto'>
         <h3 class='txt-m txt-bold mb6'>Title here</h3>
         <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
       </div>
-      <footer class='flex-child p12 bg-gray-faint txt-s'>
+      <footer class='p12 bg-gray-faint txt-s'>
         Footer content here
       </footer>
     </div>
@@ -20,11 +20,11 @@ const floatingSidebar = `<div class='viewport-full relative clip'>
   <div class='bg-darken10 viewport-twothirds viewport-full-ml absolute top left right bottom'></div>
   <div class='absolute top-ml left bottom z1 w-full w300-ml p12-ml'>
     <div class='flex-parent flex-parent--column viewport-third h-auto-ml hmax-full bg-white round-ml shadow-darken5'>
-      <div class='flex-parent flex-parent--column p12 scroll-auto'>
+      <div class='p12 scroll-auto'>
         <h3 class='txt-m txt-bold mb6'>Title here</h3>
         <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Sed posuere consectetur est at lobortis. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
       </div>
-      <footer class='flex-child p12 bg-gray-faint round-b-ml txt-s'>
+      <footer class='p12 bg-gray-faint round-b-ml txt-s'>
         Footer content here
       </footer>
     </div>
