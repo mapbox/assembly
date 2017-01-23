001b6eac5ce2a947d40a1f13423420626e57f75b
Dana Sulit <dana.sulit@mapbox.com>
2017-01-23 14:52:53 -0500

    use event delegation in expander.js

--- site/js/expander.js
+++ site/js/expander.js
@@ -1,25 +1,14 @@
-// 	event handlers on each button
+// 	'See All' button event handlers on Documentation page
 //	on click hide elem, show all
 
-const buttons = document.querySelectorAll('button[id^="expandButton-"]');
-let i;
-
-for (i = 0; i < buttons.length; i++) {
-  buttons[i].addEventListener('click', function () {
-
-    const classId = this.id.replace('expandButton-', '');
-    const expanded = document.getElementById('expanded-' + classId);
-    const collapsed = document.getElementById('collapsed-' + classId);
-
-    if (expanded.className === 'none') {
-      expanded.className = 'inline';
-      collapsed.className = 'none';
-      this.textContent = 'See fewer';
-    } else if (expanded.className === 'inline') {
-      expanded.className = 'none';
-      collapsed.className = 'inline';
-      this.textContent = 'See all';
+document.addEventListener('click', (e) => {
+  if (e.target.getAttribute('data-button-expand')) {
+    if (e.target.parentNode.children[0].classList.contains('none')){
+      e.target.textContent = 'See fewer';
+    } else {
+      e.target.textContent = 'See all';
     }
-
-  });
-}
+    e.target.parentNode.children[0].classList.toggle('none');
+    e.target.parentNode.children[1].classList.toggle('none');
+  }
+});
