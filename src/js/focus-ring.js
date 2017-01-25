eca504066c90cfd3bb83a4eda3671ce133093387
David Clark <dclark@mapbox.com>
2017-01-25 11:24:02 -0700

    Use data attribute instead of focus-control classes
    Closes #674

--- src/js/focus-ring.js
+++ src/js/focus-ring.js
@@ -1,17 +1,17 @@
 /* eslint-disable */
 (function () {
-  document.documentElement.classList.add('focus-control');
-  var activeClass = 'focus-control--visible';
+  var attributeName = 'data-assembly-focus-control';
+  document.documentElement.setAttribute(attributeName, 'not-visible');
   var on = false;
   document.addEventListener('mousedown', function () {
     if (on === false) return;
     on = false;
-    document.documentElement.classList.remove(activeClass);
+    document.documentElement.setAttribute(attributeName, 'not-visible');
   });
   document.addEventListener('keydown', function (event) {
     if (on === true) return;
     if (event.key !== 'Tab' && event.keyCode !== 9) return;
     on = true;
-    document.documentElement.classList.add(activeClass);
+    document.documentElement.setAttribute(attributeName, 'visible');
   });
 }());
