f0489b11f28655275acb1d0b25a7582c7fb59c25
Sarah Kleinman <kleinman.sarah@gmail.com>
2017-01-25 14:57:21 -0500

    Switch right and left

--- site/examples/tooltips.js
+++ site/examples/tooltips.js
@@ -6,12 +6,12 @@ const basic = `<div class='flex-parent-inline flex-parent--center-main flex-pare
   <span class='triangle triangle--d'></span>
 </div>`;
 
-const basic_left = `<div class='flex-parent-inline flex-parent--center-cross'>
+const basic_right = `<div class='flex-parent-inline flex-parent--center-cross'>
   <span class='triangle triangle--l'></span>
   <div class='bg-darken75 color-white align-center col col--12 p6 round txt-bold txt-s'>Hello world!</div>
 </div>`;
 
-const basic_right = `<div class='flex-parent-inline flex-parent--center-cross'>
+const basic_left = `<div class='flex-parent-inline flex-parent--center-cross'>
   <div class='bg-darken75 color-white align-center col col--12 p6 round txt-bold txt-s'>Hello world!</div>
   <span class='triangle triangle--r'></span>
 </div>`;
@@ -36,13 +36,13 @@ class ExampleTooltips extends React.Component {
         </h2>
         <HtmlExample code={basic} />
         <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
-          Basic tooltip (left)
-        </h2>
-        <HtmlExample code={basic_left} />
-        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
           Basic tooltip (right)
         </h2>
         <HtmlExample code={basic_right} />
+        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
+          Basic tooltip (left)
+        </h2>
+        <HtmlExample code={basic_left} />
         <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
           Tooltip with close button
         </h2>
