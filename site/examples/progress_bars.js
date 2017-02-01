d39f74ceb6ced03153dc4f2d0ba36b1874eb58b0
Sarah Kleinman <kleinman.sarah@gmail.com>
2017-01-31 19:12:54 -0500

    Add playback control example --first pass

--- site/examples/progress_bars.js
+++ site/examples/progress_bars.js
@@ -5,6 +5,19 @@ const basic = `<div class='h12 bg-darken10 relative round-full'>
   <div class='absolute h12 bg-green-light round-full' style='width:50%;'></div>
 </div>`;
 
+const playback = `<div class='flex-parent flex-parent--row flex-parent--center-cross border border--gray-light round-t'>
+    <div class='flex-child py12 pr6 pl12 border-r bg-gray-dark cursor-pointer'>
+      <span class='block triangle-l triangle-l--r color-green'></span>
+    </div>
+    <div class='flex-child flex-child--grow p18 bg-blue'>
+      <div class='relative w-full h12 bg-darken10 round-full'>
+        <div class='absolute top right bottom left m3 mh6 bg-purple-light round-full' style='width:50%;'>
+          <span class='absolute top right w30 h6 bg-red round-full'></span>
+        </div>
+      </div>
+    </div>
+  </div>`;
+
 class ExampleProgressBars extends React.Component {
   render() {
     return (
@@ -14,6 +27,12 @@ class ExampleProgressBars extends React.Component {
         </h2>
         <div className='mb24'>
           <HtmlExample code={basic} />
+        </div>
+        <h2 className='border-b border--2 border--gray-faint pb6 mt48 mb24 txt-l txt-bold'>
+          Playback controls
+        </h2>
+        <div className='mb24'>
+          <HtmlExample code={playback} />
         </div>
       </div>
     );
