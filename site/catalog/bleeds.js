ec5c8d4a7d0449d1442d5d42356644dd275a6ae5
David Clark <david.dave.clark@gmail.com>
2017-03-09 08:51:18 -0700

    Add bleed media queries and unbleed classes (#758)
    Closes #742.

---
+++ site/catalog/bleeds.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+class Bleeds extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2 className='border-b border--2 border--gray-faint pb6 mt72 mb24 txt-l txt-bold'>
+          Bleeds
+        </h2>
+
+        <div className='bg-darken10 flex-parent flex-parent--center-main clip'>
+          <div className='w180 p12 bg-darken10'>
+            <div className='bg-darken10 bleed-r'>Right bleed</div>
+          </div>
+        </div>
+        <div className='bg-darken10 flex-parent flex-parent--center-main clip'>
+          <div className='w180 p12 bg-darken10'>
+            <div className='bg-darken10 bleed-l align-r'>Left bleed</div>
+          </div>
+        </div>
+        <div className='bg-darken10 flex-parent flex-parent--center-main clip'>
+          <div className='w180 p12 bg-darken10'>
+            <div className='bg-darken10 bleed-r-mxl'>Right bleed at xl</div>
+          </div>
+        </div>
+        <div className='bg-darken10 flex-parent flex-parent--center-main clip'>
+          <div className='w180 p12 bg-darken10'>
+            <div className='bg-darken10 bleed-r unbleed-mxl'>Right bleed until xl</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export { Bleeds };
