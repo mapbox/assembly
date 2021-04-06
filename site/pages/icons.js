0d7d80dbbf2ae80caae1f8fcc914e8fcdaef7d78
Saman Bemel-Benrud <samanpwbb@gmail.com>
2021-04-06 16:13:32 -0700

    Add imperative focus API and improve documentation around assembly.js (#979)
    * Add imperative focus API and improve documentation around assembly.js
    
    closes #953
    
    * Add changelog

--- site/pages/icons.js
+++ site/pages/icons.js
@@ -34,56 +34,6 @@ export default class Icons extends React.Component {
           </p>
         </div>
         <div className="flex-parent flex-parent--wrap txt-mono">{iconEls}</div>
-
-        <h2 className="pt24 txt-bold txt-uppercase mb12 mt24">
-          Programmatically work with icons in JavaScript
-        </h2>
-        <div className="mb24 prose">
-          To add icons to the page or edit them programmatically, you must use
-          special DOM methods that handle XML namespaces. To facilitate this,{' '}
-          <code>assembly.js</code> exposes a few functions on a global{' '}
-          <code>Assembly</code> namespace.
-        </div>
-        <h3 className="mb12 mt24 txt-bold">
-          <code className="txt-code">
-            Assembly.createIcon(iconName: string): SVGElement
-          </code>
-        </h3>
-        <div className="prose">
-          <p>
-            Returns an SVG element containing a <code>{'<use>'}</code> element
-            referencing the designated icon.
-          </p>
-          <p>
-            Throws an error if the designated icon does not exist in Assembly.
-          </p>
-        </div>
-        <h3 className="mb12 mt24 txt-bold">
-          <code className="txt-code">
-            Assembly.changeIcon(iconEl: SVGElement, iconName: string):
-            SVGElement
-          </code>
-        </h3>
-        <div className="prose">
-          <p>
-            Given an icon SVG element (<em>not</em> the <code>{'<use>'}</code>{' '}
-            element inside it), such as what <code>Assembly.createIcon</code>{' '}
-            returns, changes the icon and returns the SVG element.
-          </p>
-          <p>
-            Throws an error if the designated icon does not exist in Assembly.
-          </p>
-        </div>
-        <h3 className="mb12 mt24 txt-bold">
-          <code className="txt-code">
-            Assembly.iconExists(iconName: string): boolean
-          </code>
-        </h3>
-        <div className="prose">
-          <p>
-            Returns a boolean indicating whether an icon exists in Assembly.
-          </p>
-        </div>
       </Page>
     );
   }
--- src/js/focus-control.js
+++ src/js/focus-control.js
@@ -1,16 +1,32 @@
 (function() {
+  var Assembly = (window.Assembly = window.Assembly || {});
+  var on = false;
   var attributeName = 'data-assembly-focus-control';
   document.documentElement.setAttribute(attributeName, 'not-visible');
-  var on = false;
-  document.addEventListener('mousedown', function() {
+
+  function enable() {
+    if (on === true) return;
+    on = true;
+    document.documentElement.setAttribute(attributeName, 'visible');
+  }
+  function disable() {
     if (on === false) return;
     on = false;
     document.documentElement.setAttribute(attributeName, 'not-visible');
+  }
+
+  // Listeners
+  document.addEventListener('mousedown', function() {
+    disable();
   });
   document.addEventListener('keydown', function(event) {
-    if (on === true) return;
     if (event.key !== 'Tab' && event.keyCode !== 9) return;
-    on = true;
-    document.documentElement.setAttribute(attributeName, 'visible');
+    enable();
   });
+
+  // Imperative API
+  Assembly.setFocusState = function(value) {
+    if (value === undefined) value = !on;
+    value ? enable() : disable();
+  };
 })();
