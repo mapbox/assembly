4499f15c9c0bb395db84e51cee16a70b31d99190
Katy DeCorah <decorah@mapbox.com>
2021-05-20 10:49:09 -0400

    Disable import/no-unresolved in application_wrapper.js

--- site/application_wrapper.js
+++ site/application_wrapper.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 if (typeof window !== 'undefined') {
-  import(/* webpackChunkName: "assembly-js" */ '../dist/assembly.js');
+  import(/* webpackChunkName: "assembly-js" */ '../dist/assembly.js'); // eslint-disable-line import/no-unresolved
 }
 
 export default class ApplicationWrapper extends React.Component {
